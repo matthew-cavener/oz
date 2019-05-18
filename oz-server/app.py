b56e86b946b024e8c363175e83b5635fe5095f2c
Matthew Cavener <matthew.cavener@gmail.com>
2019-05-18 08:34:07 -0700

    test USE lite, ~500 MB used, and 4922 ms to embed/cluster 1276 messages.

--- oz-server/app.py
+++ oz-server/app.py
@@ -5,6 +5,7 @@ import json
 
 import tensorflow as tf
 import tensorflow_hub as hub
+import sentencepiece as spm
 import numpy as np
 
 from collections import defaultdict
@@ -13,22 +14,45 @@ from os import remove
 
 logger = logging.getLogger(__name__)
 
-module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
+# module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
 # module_url = "https://tfhub.dev/google/universal-sentence-encoder/2"
+module_url = "https://tfhub.dev/google/universal-sentence-encoder-lite/2"
+
+with tf.Session() as sess:
+    module = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-lite/2")
+    spm_path = sess.run(module(signature="spm_path"))
+
+sp = spm.SentencePieceProcessor()
+sp.Load(spm_path)
+logger.debug("SentencePiece model loaded at {}.".format(spm_path))
 
 # Create graph and finalize (finalizing optional but recommended).
 g = tf.Graph()
 with g.as_default():
     # We will be feeding 1D tensors of text into the graph.
-    text_input = tf.placeholder(dtype=tf.string, shape=[None])
+    input_placeholder = tf.sparse_placeholder(tf.int64, shape=[None, None])
     embed = hub.Module(module_url)
-    embedded_text = embed(text_input)
+    embedded_text = embed(inputs=dict(
+        values=input_placeholder.values,
+        indices=input_placeholder.indices,
+        dense_shape=input_placeholder.dense_shape))
     init_op = tf.group([tf.global_variables_initializer(), tf.tables_initializer()])
 g.finalize()
 
 # Create session and initialize.
 session = tf.Session(graph=g)
 session.run(init_op)
+
+def process_to_IDs_in_sparse_format(sp, sentences):
+  # An utility method that processes sentences with the sentence piece processor
+  # 'sp' and returns the results in tf.SparseTensor-similar format:
+  # (values, indices, dense_shape)
+  ids = [sp.EncodeAsIds(x) for x in sentences]
+  max_len = max(len(x) for x in ids)
+  dense_shape=(len(ids), max_len)
+  values=[item for sublist in ids for item in sublist]
+  indices=[[row,col] for row in range(len(ids)) for col in range(len(ids[row]))]
+  return (values, indices, dense_shape)
 
 @hug.get('/health')
 def health():
@@ -41,7 +65,14 @@ def label(body, metric):
     utterances = body['utterances']
     keys = ['text', 'intent', 'confidence']
     common_examples = []
-    embeddings = session.run(embedded_text, feed_dict={text_input: utterances}).tolist()
+    values, indices, dense_shape = process_to_IDs_in_sparse_format(sp, utterances)
+
+    embeddings = session.run(
+        embedded_text,
+        feed_dict={input_placeholder.values: values,
+                   input_placeholder.indices: indices,
+                   input_placeholder.dense_shape: dense_shape}).tolist()
+
     clusterer = hdbscan.HDBSCAN(
         metric=metric,
         min_cluster_size=5,
